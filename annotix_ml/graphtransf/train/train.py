9d0ac6cd80b4afcc9b2c43c469fd35fe54e4b2ed
etienneguevel <etienneguev@gmail.com>
2026-02-18 12:35:48 +0100

    fix: restrain profiler 1st cycle

--- annotix_ml/graphtransf/train/train.py
+++ annotix_ml/graphtransf/train/train.py
@@ -354,7 +354,7 @@ def train(cfg):
 
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3),
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
         on_trace_ready=torch.profiler.tensorboard_trace_handler(
             cfg.train.save_path + "/log/profile"
         ),
